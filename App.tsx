b166389605d2c2b0f2ba8fa920aaabb2ba5e994c
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2026-09-17 18:38:41 -0300

    feat: initialize Expo React Native project with NativeWind and Tailwind CSS

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import './global.css';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white" style={styles.container}>
+      <Text className="text-base font-medium text-slate-800">Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
